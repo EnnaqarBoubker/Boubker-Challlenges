765f7b3a4340b4874e0ca907398088b6bb7888a1
EnnaqarBoubker <93933750+EnnaqarBoubker@users.noreply.github.com>
2021-11-10 18:17:58 +0100

    Update First_challenges.c

--- First_challenges.c
+++ First_challenges.c
@@ -3,21 +3,21 @@
 
 int main () {
 	
-    char N[10], P[10], S[10], T[10];
-    int A;
+    char Nom[10], Prenom[10], Sexe[10], Tele[10];
+    int Age;
 	
 	
     printf("donner moi votre Nom :");
-     scanf("%s", &N);
+     scanf("%s", &Nom);
     printf("donner moi votre Prenom :");
-     scanf("%s", &P);
+     scanf("%s", &Prenom);
     printf("donner moi votre age :");
-     scanf("%d", &A);
+     scanf("%d", &Age);
     printf("donner moi votre Sexe :");
-     scanf("%s", &S);
+     scanf("%s", &Sexe);
     printf("donner moi votre Telephone :");
-     scanf("%s", &T);
-    printf("Mon Nom est: %s; mon Prenom est : %s; mon age est :%d; mon sexe :%s; mon telephone est :%s" ,N,P,A,S,T);
+     scanf("%s", &Tele);
+    printf("Mon Nom est: %s; mon Prenom est : %s; mon age est :%d; mon sexe :%s; mon telephone est :%s" ,Nom,Prenom,Age,Sexe,Tele);
 
     
     
