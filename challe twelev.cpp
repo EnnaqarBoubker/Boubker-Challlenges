00a7c8258ab05d17ef21fdd06c2285e45961066c
EnnaqarBouber <ennaqar.boubker5@gmail.com>
2021-11-14 16:43:17 +0100

    challenge

--- challe twelev.cpp
+++ challe twelev.cpp
@@ -1,18 +1,23 @@
+
 #include<stdio.h>
 #include<stdlib.h>
 int main()
 {
-    int r,a,b;
-    printf("Donner un entier positif:\n");
-    scanf("%d",&a);
-    b=0;
-    while(a>0)
-    {
-        r=a%10;
-        b=10*b+r;
-        a=a/10;
-    }
-    printf("l'inverse de l'entier donne en entrée est %d\n",b);
-    system("pause");
-    return 0;
+	int A, B, C;
+	
+	printf("donner moi un nomber Entire : \n");
+	 scanf("%d", &A);
+	 
+	
+	B = (A % 10) * 100;
+	A= A / 10;
+	B = B + (A % 10) * 10;
+	A = A / 10;
+	B = B + A;
+	C = A;
+	printf(" L'inverce %d  est le nomber %d", C,B );
+	
+	
+	return 0;
+	
 }
