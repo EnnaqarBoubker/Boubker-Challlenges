d74e378d050e71397520243a52d9d4b9b01696f8
EnnaqarBoubker <ennaqar.boubker5@gmail.com>
2021-11-19 09:23:33 +0100

    modification challenge 3 et push challenge 4

--- des boucles/challenge 3.c
+++ des boucles/challenge 3.c
@@ -1,33 +1,31 @@
+
 #include<stdio.h>
 #include<stdlib.h>
 
 int main()
 {
-	int N, i;
-	int Z = 0;
+	int N, i, A;
+	
 	
 	printf("donner moi un nomber \n");
 	 scanf("%d", &N);
-
 	 
-	for(i=2; i<= N / 2;i++)
-	{
-	
-		if( N % i == 0){
-	    	Z = 2;
-	    	break;
-	    }
-		
-	}	
-		if(Z == 0){
-	
-		    printf("le nombre premier");	}
-		else{
-	
-		    printf("le nombre pas premier");	}
+	 
+	 for (i= 2; i<= N/2; i++)
+	 
+	     A = N % i;
+	     
+	    if(A == 0){
+	    	printf("le nombre pas premier");
+		}
+		else {
+			printf("le nombre premier");
+		}
+
 	
 		   return 0;
 }
+
 
 
 
--- des boucles/challenge 4.c
+++ des boucles/challenge 4.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 int main()
 {
-    int N ,i, A = 0;
+    int N /* nomber de l'utilisateur*/ , A ;
     
     
     printf("Donner moi un nombre : ");
@@ -12,12 +12,12 @@ int main()
    
     while(N > 0)
     {
-        i = N % 10;
-        //A = (10 * A)+ i;
+        
+        A = (10 * A)+ (N % 10);
         N = N / 10;
     }
     
-    printf("l inverse est %d\n",i);
+    printf("l inverse est %d\n",A);
     
     
     return 0;
